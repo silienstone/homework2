692d108e116c4652e27777a7237d5a2fdf920f59
Student Generic Account <ADStudent@auaad.aua.am>
2017-09-09 15:35:47 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,12 @@
+console.log(21+12);
+alert("oh hey, whats up?");
+const a=10;
+const b=8;
+ console.log(a*b/2) ;
+
+const x=2
+const y=6
+if(x>y) {console.log(x-y);
+}
+else if(x===y) {console.log("try with different numbers")}
+	else { alert("error,get outta here")}
